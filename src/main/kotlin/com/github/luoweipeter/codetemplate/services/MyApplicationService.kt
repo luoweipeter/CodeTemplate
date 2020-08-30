a6dae4e52b4404adaf76938073d126340c92221d
GitHub Action <action@github.com>
2020-08-30 04:01:28 +0000

    Template cleanup

--- src/main/kotlin/com/github/luoweipeter/codetemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/luoweipeter/codetemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.luoweipeter.codetemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.luoweipeter.codetemplate.MyBundle
 
 class MyApplicationService {
 
